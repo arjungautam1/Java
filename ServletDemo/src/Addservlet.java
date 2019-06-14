40044421afb4d6afd00a405a9b8f5122e5ceb678
Laserarjun876 <laserarjun876@gmail.com>
2019-06-14 18:43:36 +0545

    first servlet demo

---
+++ ServletDemo/src/Addservlet.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.*;
+public class Addservlet extends HttpServlet{
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		int k=i+j;
+		PrintWriter out=res.getWriter();
+		out.println("sum is "+k);
+		
+	}
+
+}
